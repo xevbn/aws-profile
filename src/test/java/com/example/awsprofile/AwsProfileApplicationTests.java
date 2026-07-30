78f9503f0a04a125b343245c75d9731c6cde7965
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-07-30 16:16:56 +0900

    Initial commit

---
+++ src/test/java/com/example/awsprofile/AwsProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
