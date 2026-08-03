57df8ab3e4a8dc5530fb07359202c0c0c295a973
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-08-03 19:59:18 +0900

    test

--- src/test/java/com/example/awsprofile/AwsProfileApplicationTests.java
+++ src/test/java/com/example/awsprofile/AwsProfileApplicationTests.java
@@ -1,10 +1,14 @@
 package com.example.awsprofile;
 
+import io.awspring.cloud.s3.S3Template;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class AwsProfileApplicationTests {
+    @MockitoBean
+    private S3Template s3Template;
 
     @Test
     void contextLoads() {
