651db4601123208995933c1cf5f01e37266969f0
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-08-04 11:02:20 +0900

    test

--- src/test/java/com/example/awsprofile/AwsProfileApplicationTests.java
+++ src/test/java/com/example/awsprofile/AwsProfileApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.awsprofile;
 
+import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
 import io.awspring.cloud.s3.S3Template;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {S3AutoConfiguration.class})
 class AwsProfileApplicationTests {
     @MockitoBean
     private S3Template s3Template;
